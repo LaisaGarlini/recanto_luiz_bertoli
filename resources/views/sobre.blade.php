149c00d5331a947238a483db5e2b93b014db1fa4
aagablm <ahgabilima@gmail.com>
2024-06-20 20:42:12 -0300

    Update sobre.blade.php

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -1,28 +1,28 @@
 @extends('layout.app')
-@section('title',$title)
+@section('title', $title)
 @section('content')
-    <h1>Bem-Vindo ao Sobre</h1>
 
-    <div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://scontent.flaj2-1.fna.fbcdn.net/v/t39.30808-6/444469135_850584273769568_4424128133179611985_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=auuQ2X5YdBoQ7kNvgEeOmYt&_nc_ht=scontent.flaj2-1.fna&oh=00_AYA5h7ykpqBhsKFIfDFVNpuzAkVcL4wzr1Y0fZ6-Ri-iSw&oe=667A7B98" class="d-block w-100" alt="...">
+
+<div class="row featurette">
+    <div class="col-md-7">
+        <h1 class="featurette-heading fw-normal lh-1">Sobre <span class="text-body-secondary"></span></h1>
+        <p class="lead">Administrado pela Congregação das Pequenas Missionárias de Maria Imaculada o Recanto Luiz
+            Bertoli é um Instituto de Longa Permanência para Idosos – seu público alvo é idosos de 60 anos ou mais, de
+            ambos os sexos, com diversos graus de dependência.
+        </p>
     </div>
-    <div class="carousel-item">
-      <img src="https://scontent.flaj2-1.fna.fbcdn.net/v/t39.30808-6/444469135_850584273769568_4424128133179611985_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=auuQ2X5YdBoQ7kNvgEeOmYt&_nc_ht=scontent.flaj2-1.fna&oh=00_AYA5h7ykpqBhsKFIfDFVNpuzAkVcL4wzr1Y0fZ6-Ri-iSw&oe=667A7B98" class="d-block w-100" alt="...">
+    <div class="col-md-5">
+        <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+            height="500"
+            src="https://scontent.flaj2-1.fna.fbcdn.net/v/t39.30808-6/439026203_825547479606581_3355319321593237389_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Fp4RIKjPgDQQ7kNvgFZJTdK&_nc_ht=scontent.flaj2-1.fna&oh=00_AYD05M3_zgKMqs6WMSxkRf0yyTkIojnMEqjaPmliQquH4g&oe=667A8BB5"
+            role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%"
+            fill="var(--bs-secondary-color)" dy=".3em"></text></img>
     </div>
-    <div class="carousel-item">
-      <img src="https://scontent.flaj2-1.fna.fbcdn.net/v/t39.30808-6/444469135_850584273769568_4424128133179611985_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=auuQ2X5YdBoQ7kNvgEeOmYt&_nc_ht=scontent.flaj2-1.fna&oh=00_AYA5h7ykpqBhsKFIfDFVNpuzAkVcL4wzr1Y0fZ6-Ri-iSw&oe=667A7B98" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
+
+
+
 
 @endsection
